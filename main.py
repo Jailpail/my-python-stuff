45a70d9b70ffae067f06f7466aed998419505d3d
gerko <gerkoton@gmail.com>
2022-10-16 14:38:33 +0300

    the joe edition

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("hello but edited")
+from random import *
+joe = random()
+if joe == 1:
+    print("hello this is joe 1")
+else:
+    print("hello this is joe 2 ( real one , 1st doesnt exist )")
