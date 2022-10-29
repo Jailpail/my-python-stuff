3ac9169db97de1a88ce18159c1b48778e102fd23
gerko <gerkoton@gmail.com>
2022-10-29 16:15:03 +0300

    CAN WE GET MUCH HIGHER

---
+++ nothing can get worse ,right.py
@@ -0,0 +1,34 @@
+class ahyes:
+    def __init__(self):
+     super().__init__()
+     self.the = "the"
+class ahyes1:
+    def __init__(self):
+     super().__init__()
+     self.one = "one"
+class ahyes2:
+    def __init__(self):
+     super().__init__()
+     self.piece = "piece"
+class ahyes3:
+    def __init__(self):
+     super().__init__()
+     self.iss = "is"
+class ahyes4:
+    def __init__(self):
+     super().__init__()
+     self.real = "real"
+class ahyesfinal:
+    def __init__(self):
+     super().__init__()
+     self.canwegetmuchhigher = "CAN WE GET MUCH HIGHER , SO HIGH , OH OH OH"
+class theonepiece(ahyes, ahyes1, ahyes2, ahyes3, ahyes4 ,ahyesfinal):
+
+    def theonepieceisreal(self):
+      self.divideonepiece = ","
+      print(self.the,self.one,self.piece,self.divideonepiece,self.the,self.one,self.piece , self.iss,self.real,self.canwegetmuchhigher)
+realpiece = theonepiece()
+realpiece.theonepieceisreal()
+
+
+### this is a reference to the me of one piece and Dark Fantasy by Kanye West.
